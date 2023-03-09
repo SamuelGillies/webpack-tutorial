88f225c89d4c36650b860ce1e41a83abb004979e
Sam Gillies <sam@samgillies.com>
2023-03-09 14:59:52 +0000

    cleaned up code to default state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import _ from 'lodash';
-import myName from './myName.js'; 
+
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script - removed for Odin Project part 2 of tutorial, importing own module
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
-
-  // use your function! 
-  element.textContent = myName('Cody'); 
+  element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
 
   return element;
 }
