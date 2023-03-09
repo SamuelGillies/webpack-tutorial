7fc501a681f5186401f1de5ba69dbeec8aa2f997
Sam Gillies <sam@samgillies.com>
2023-03-09 13:49:50 +0000

    Odin Project additional tutorial steps added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
+import myName from './myName.js'; 
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script - removed for Odin Project part 2 of tutorial, importing own module
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
+
+  // use your function! 
+  element.textContent = myName('Cody'); 
 
   return element;
 }
